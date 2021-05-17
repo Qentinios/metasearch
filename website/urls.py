c0695066544d895c6f7ce76ab06c9104fa459c1e
Adrian Czok <qentinios@gmail.com>
2021-05-17 10:35:38 +0200

    Home & about page templates

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from website.views import HomeView
+from website.views import HomeView, SearchView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
+    path('search', SearchView.as_view(), name='search'),
+    path('about', AboutView.as_view(), name='about'),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class SearchView(TemplateView):
+    template_name = 'search.html'
